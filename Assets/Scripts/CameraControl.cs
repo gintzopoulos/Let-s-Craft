2dc0e1778e1bafec10ebcb632c54b1dac430150d
Grégoire Intzopoulos <gregoire.intzopoulos1@etu.uqac.ca>
2020-03-28 12:43:53 -0400

    Ajout déplacement caméra

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+
+public class CameraControl : MonoBehaviour
+{
+    public float vitDéfilement = 10f;
+    private bool peutSeDéplacer = true;
+    public SpriteRenderer limiteGauche;
+    public SpriteRenderer limiteDroite;
+    
+
+    void Start()
+    {
+
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("a") && pos.x > limiteGauche.transform.position.x)
+        { pos.x -= vitDéfilement * Time.deltaTime;}
+
+        if (Input.GetKey("d") && pos.x < limiteDroite.transform.position.x)
+        { pos.x += vitDéfilement * Time.deltaTime;}
+
+        transform.position = pos;
+
+        
+    }
+
+
+}
